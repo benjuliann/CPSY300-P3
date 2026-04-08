5a63384d7d7c79c494db6e419eb93ae6a6ac8a26
launchland <benjulian.noel@gmail.com>
2026-04-08 12:13:19 -0600

    trustHost set to true
    Adding this line onto auth.js to allow NextAuth v5 to trust the production domain

--- auth.js
+++ auth.js
@@ -3,6 +3,8 @@ import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
+
   session: {
     strategy: "jwt",
   },
